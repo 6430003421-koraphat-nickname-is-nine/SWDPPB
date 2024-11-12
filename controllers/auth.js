cef4a7598fa200d2775f7b45691c27d41cdf4f47
koraphat <koraphat.lum@gmail.com>
2024-11-12 13:16:28 +0700

    add getUserById

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,6 +88,29 @@ exports.getMe = async (req, res, next) => {
   });
 };
 
+//@desc   Get  User name by id
+//@route  get /api/v1/auth/:id
+//@access Private
+exports.getUser = async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: `No user with the id of ${req.params.id}`,
+      });
+    }
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot find User",
+    });
+  }
+};
+
 //@desc   Log user out / clear cookie
 //@route  GET /api/v1/auth/logout
 //@access Private
